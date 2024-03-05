827c0959c0871061f52b1b11382f14ebb58edec5
manjaka13 <manjaka.rajaonson@gmail.com>
2024-03-05 13:38:56 +0300

    setup bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/index.css";
-import App from "@/components/App";
+import { App } from "@/components/App";
 
 /**
  * Entry point
